851f165b3a9091dc4eee67d2797ee2312f02d6a5
augusthur <al.mathurin@hotmail.com>
2016-08-07 17:33:21 -0300

    Agregado campo birthday como fecha

--- models/Preusuario.php
+++ models/Preusuario.php
@@ -2,6 +2,7 @@
 
 class Preusuario extends Eloquent {
     //protected $table = 'preusuarios';
+    protected $dates = ['birthday'];
     protected $visible = ['id', 'nombre', 'apellido', 'emailed_token'];
     protected $fillable = ['email'];
 }
