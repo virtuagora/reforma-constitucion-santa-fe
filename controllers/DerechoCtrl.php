2638e3e7bc8256fa029d0ef2f30d9a591591fb22
augusthur <al.mathurin@hotmail.com>
2016-08-08 14:43:33 -0300

    modificar derecho listo

--- controllers/DerechoCtrl.php
+++ controllers/DerechoCtrl.php
@@ -49,28 +49,66 @@ class DerechoCtrl extends Controller {
         $contenido = new Contenido;
         $contenido->titulo = $vdt->getData('titulo');
         $contenido->puntos = 0;
-        $contenido->categoria_id = $vdt->getData('categoria');
+        $contenido->categoria_id = 1;
         $contenido->autor()->associate($autor);
         $contenido->contenible()->associate($derecho);
         $contenido->save();
-        
         if ($derecho->imagen) {
-            $dir = __DIR__ . '/../public/img/derecho';
-            if (!is_dir($dir)) {
-                mkdir($dir, 0777, true);
+            $subida = subirImagen($derecho->id);
+            if (!$subida) {
+                throw new TurnbackException('Error al cargar la imagen');
             }
-            $storage = new \Upload\Storage\FileSystem($dir, true);
-            $file = new \Upload\File('archivo', $storage);
-            $file->setName($derecho->id);
-            $file->addValidations([
-                new \Upload\Validation\Mimetype(['image/jpg', 'image/jpeg']),
-                new \Upload\Validation\Size('2M')
-            ]);
-            $file->upload();
         }
-        
         $this->flash('success', 'El derecho se creó exitosamente.');
         $this->redirectTo('shwDerecho', ['idDer' => $derecho->id]);
+    }
+    
+    public function verModificar($idDer) {
+        $vdt = new Validate\QuickValidator([$this, 'notFound']);
+        $vdt->test($idDer, new Validate\Rule\NumNatural());
+        $categorias = Categoria::all()->toArray();
+        $derecho = Derecho::with('contenido')->findOrFail($idDer);
+        $contenido = $derecho->contenido;
+        $datos = array_merge($contenido->toArray(), $derecho->toArray());
+        $this->render('lpe/contenido/derecho/editar.twig', [
+            'derecho' => $derecho,
+            'categorias' => $categorias
+        ]);
+    }
+
+    public function modificar($idDer) {
+        $vdt = new Validate\QuickValidator([$this, 'notFound']);
+        $vdt->test($idDer, new Validate\Rule\NumNatural());
+        $derecho = Derecho::with('contenido')->findOrFail($idDer);
+        $contenido = $derecho->contenido;
+        $usuario = $this->session->getUser();
+        $req = $this->request;
+        $vdt = $this->validarDerecho($req->post());
+        $derecho->descripcion = $vdt->getData('descripcion');
+        $derecho->video = $vdt->getData('video');
+        $imagenSubida = is_uploaded_file($_FILES['archivo']['tmp_name']);
+        $derecho->imagen = ($derecho->imagen || $imagenSubida);
+        $derecho->save();
+        $contenido->titulo = $vdt->getData('titulo');
+        $contenido->save();
+        $accNew = $vdt->getData('secciones');
+        $accOld = $derecho->secciones;
+        $i = 0;
+        foreach ($accOld as $accion) {
+            if (isset(accNew[$i])) {
+                $accion->descripcion = accNew[$i];
+                $i++;
+                $accion->save();
+            }
+        }
+        if ($imagenSubida) {
+            $subida = subirImagen($derecho->id);
+            if (!$subida) {
+                throw new TurnbackException('Error al cargar la imagen');
+            }
+        }
+        $this->flash('success', 'Los datos del derecho fueron modificados exitosamente.');
+        $this->redirectTo('shwDerecho', array('idDer' => $idDer));
     }
     
     public function votar($idSec) {
@@ -99,18 +137,35 @@ class DerechoCtrl extends Controller {
         $vdt->addRule('titulo', new Validate\Rule\MinLength(1))
             ->addRule('titulo', new Validate\Rule\MaxLength(128))
             ->addRule('descripcion', new Validate\Rule\MinLength(4))
-            // ->addRule('descripcion', new Validate\Rule\MaxLength(2048))
-            ->addRule('categoria', new Validate\Rule\NumNatural())
-            ->addRule('categoria', new Validate\Rule\Exists('categorias'))
             ->addRule('video', new Validate\Rule\MinLength(8))
             ->addRule('video', new Validate\Rule\MaxLength(16))
             ->addRule('secciones', new Validate\Rule\MinLength(4))
             ->addRule('secciones', new Validate\Rule\MaxLength(1024))
-            ->addFilter('secciones', FilterFactory::explode('&&'))
-            ->addFilter('tags', FilterFactory::explode(','));
+            ->addFilter('secciones', FilterFactory::explode('&&'));
         if (!$vdt->validate($data)) {
             throw new TurnbackException($vdt->getErrors());
         }
         return $vdt;
+    }
+    
+    private function subirImagen($nombre) {
+        $exito = true;
+        $dir = __DIR__ . '/../public/img/derecho';
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+        $storage = new \Upload\Storage\FileSystem($dir, true);
+        $file = new \Upload\File('archivo', $storage);
+        $file->setName($nombre);
+        $file->addValidations([
+            new \Upload\Validation\Mimetype(['image/jpg', 'image/jpeg']),
+            new \Upload\Validation\Size('2M')
+        ]);
+        try {
+            $file->upload();
+        } catch (\Exception $e) {
+            $exito = false;
+        }
+        return $exito;
     }
 }
